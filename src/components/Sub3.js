c9360cb5dd14b62592f9892bc632a262108013d1
progist <hblee@progist.co.kr>
2025-06-28 17:05:42 +0900

    모바일 메뉴 수정

--- src/components/Sub3.js
+++ src/components/Sub3.js
@@ -35,10 +35,7 @@ const ZoomBackground = styled.div`
     animation: ${zoomAnim} 1s ease-out;
 `;
 
-function isMobileDevice() {
-    return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
-        .test(navigator.userAgent);
-}
+
 
 export default function Sub3() {
 
@@ -46,130 +43,73 @@ export default function Sub3() {
     const isDesktop = useMediaQuery({query: '(min-width: 900px)'});
     const [current, setCurrent] = useState(0);
 
-    const isLandscape = useMediaQuery({ query: '(orientation: landscape)' });
-    const [mobileDevice, setMobileDevice] = useState(false);
-
-
-    useEffect(() => {
-        setMobileDevice(isMobileDevice());
-    }, []);
 
     return (
         <div
             style={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+                overflow: 'hidden',
             }}
         >
-            {/* 1) 메인 콘텐츠 영역 */}
-            <div
-                style={{
-                    flex: 1,
-                    position: 'relative',
-                    width: '100%',
-                    overflow: 'hidden',
-                }}
-            >
+
+            <div style={{height: `calc(100dvh - ${isMobile ? 227 : 290}px)`}}>
+                <div style={{display : 'flex', alignItems : 'center',   height: '100%', justifyContent : 'center',flexWrap: 'wrap'}}>
 
 
-                <div
-                    style={{
-                        height: `calc(100dvh - ${isLandscape && mobileDevice ? 150 :(isMobile ? 227 : 290)}px)`,
+                    <div style={{
+                        // paddingTop: 80,
                         display: 'flex',
                         alignItems: 'center',
                         justifyContent: 'center',
+                        backgroundColor : 'black',
+                        width : '100vw',
                         flexWrap: 'wrap',
-                    }}
-                >
-                    <div
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            flexWrap: 'wrap',
-                            backgroundColor: 'black',
-                            width: '100%',
-                            padding: !isDesktop ? 50 : 80,
-                            color: 'white',
-                            gap: !isDesktop ? 30 : 80,
-                            fontSize: !isDesktop ? 12 : 30,
-                            fontWeight: 800,
-                        }}
-                    >
-                        <div
-                            style={{
-                                textAlign: 'center',
-                                width: '100%',
-                                fontSize: 25,
-                                paddingTop: isLandscape && mobileDevice ? 30 : 0,
-                            }}
-                        >
+                        padding : !isDesktop ? 50 : 80,
+
+                        color : 'white',
+
+                        gap: !isDesktop ? 30 :  80,
+                        fontSize: !isDesktop ? 12 : 30,
+                        fontWeight: 800,
+
+                    }}>
+
+
+                        <div style={{
+                            textAlign : 'center',
+                            width : '100%',
+                            fontSize : 25
+                        }}>
                             PRODUCT BY
                         </div>
+                        <div style={{display : 'flex', alignItems : 'center', gap :!isDesktop ? 20 :  30}}>
+                            <div>
+                                주최
+                            </div>
 
-                        <div
-                            style={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                gap: !isDesktop ? 20 : 30,
-                            }}
-                        >
-                            <div>주최</div>
-                            <img
-                                src="https://sikaf.co.kr/logo.svg"
-                                height={!isDesktop ? 30 : 50}
-                                alt="SIKAF 추진위원회"
-                            />
+                            <img src="https://sikaf.co.kr/logo.svg" height={!isDesktop ? 30 : 50} alt=""/>
                         </div>
 
-                        <div
-                            style={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                gap: !isDesktop ? 20 : 30,
-                            }}
-                        >
-                            <div>주관</div>
-                            <img
-                                src="https://sikaf.co.kr/bom.png"
-                                height={!isDesktop ? 30 : 50}
-                                alt="비오앰"
-                            />
+                        <div style={{display : 'flex', alignItems : 'center', gap : !isDesktop ? 20 : 30}}>
+                            <div>
+                                주관
+                            </div>
+                            <img src="https://sikaf.co.kr/bom.png" height={!isDesktop ? 30 : 50} alt=""/>
+
+                        </div>
+                        <div style={{display : 'flex', alignItems : 'center', gap : !isDesktop ? 20 : 30}}>
+                            <div>
+                                후원
+                            </div>
+                            <img src="https://sikaf.co.kr/seoul.svg" height={isMobile ? 30 : 50} alt=""/>
                         </div>
 
-                        <div
-                            style={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                gap: !isDesktop ? 20 : 30,
-                            }}
-                        >
-                            <div>후원</div>
-                            <img
-                                src="https://sikaf.co.kr/seoul.svg"
-                                height={isMobile ? 30 : 50}
-                                alt="서울시"
-                            />
-                        </div>
                     </div>
                 </div>
             </div>
-
-            {/* 2) 겹치지 않는 하단 푸터 */}
-            <div
-                style={{
-                    height:      isLandscape &&mobileDevice ? 110 : 280,
-                    backgroundColor: '#2D2D2D',
-                    color: 'white',
-                    // display:     isLandscape &&mobileDevice ? 'flex' : '',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    fontSize: 16,
-                }}
-            >
-                <Footers/>
-            </div>
+            <Footers/>
         </div>
     );
 }
