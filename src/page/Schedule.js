259e97a5084c85427146f001a69296c0b3417cc1
progist <hblee@progist.co.kr>
2025-07-08 20:13:26 +0900

    ico 관련 파일 제거

--- src/page/Schedule.js
+++ src/page/Schedule.js
@@ -4,8 +4,8 @@ import DateList from "../components/DateList";
 import subImage from "../resources/image/sub_image2.png";
 
 export default function Schedule() {
-    const isMobile = useMediaQuery({ query: "(max-width: 500px)" });
-    const isDesktop = useMediaQuery({ query: "(min-width: 900px)" });
+    const isMobile = useMediaQuery({query: "(max-width: 500px)"});
+    const isDesktop = useMediaQuery({query: "(min-width: 900px)"});
 
     const style = {
         backgroundImage: `url(/subtitle/subtitle1.jpg)`,
@@ -18,15 +18,24 @@ export default function Schedule() {
 
     return (
         <>
-            <div style={{ height : isMobile ? 120 : (isDesktop ? 200 : 190),display : 'flex', alignItems : 'center',   marginTop : isDesktop ? 80 : 70,...style}}>
-                <div style={{paddingTop:isMobile ? 0 : 0, display : 'flex', alignItems : 'center'}}>
+            <div style={{
+                height: isMobile ? 120 : (isDesktop ? 200 : 190),
+                display: 'flex',
+                alignItems: 'center',
+                marginTop: isDesktop ? 80 : 70, ...style
+            }}>
+                <div style={{paddingTop: isMobile ? 0 : 0, display: 'flex', alignItems: 'center'}}>
                     <img src="/charactor1.svg" width={isMobile ? 80 : 150} alt=""/>
                 </div>
                 <div style={{padding: isMobile ? '100px 20px' : '120px 60px'}}>
-                    <div style={{fontSize : isMobile ? 35 :(isDesktop ?50 : 45), fontWeight : 800}}>일정안내</div>
-                    <div style={{paddingTop :isMobile ? 10 :  20, fontSize : isMobile ? 15 : (isDesktop ? 20 : 17), lineHeight : 1.5}}>
+                    <div style={{fontSize: isMobile ? 35 : (isDesktop ? 50 : 45), fontWeight: 800}}>일정안내</div>
+                    <div style={{
+                        paddingTop: isMobile ? 10 : 20,
+                        fontSize: isMobile ? 15 : (isDesktop ? 20 : 17),
+                        lineHeight: 1.5
+                    }}>
                         2025. 7. 12 SAT
-                        <br />
+                        <br/>
                         광화문 광장 놀이마당 일대(세종대왕상 앞)
                     </div>
                 </div>
@@ -40,7 +49,7 @@ export default function Schedule() {
                     padding: isMobile ? "30px 20px 35px" : "60px 60px 35px",
                 }}
             >
-                <div style={{ fontSize: 13, fontWeight: 800 }}>
+                <div style={{fontSize: 13, fontWeight: 800}}>
                     <div>행사시간 : 10:00 - 19:00</div>
                     <div>사생대회 : 13:00 - 16:30</div>
                 </div>
@@ -63,7 +72,7 @@ export default function Schedule() {
                     }}
                 >
                     2025 광복 80주년 기념
-                    <br />
+                    <br/>
                     어린이·청소년 나라사랑 아트페스티벌
                 </div>
             </div>
@@ -73,15 +82,24 @@ export default function Schedule() {
                 style={{
                     color: "black",
                     backgroundColor: "white",
-                    padding: isMobile ? "0px 20px 200px" : "0px 60px 200px",
+                    padding: isMobile ? "0px 20px 0px" : "0px 60px 50px",
                     lineHeight: 1.8,
                     fontSize: isMobile ? 12 : isDesktop ? 18 : 15,
                 }}
             >
-                <DateList />
+
+                {/*<img src="/schedule.png" width={'100%'} style={{maxWidth : 700}} alt=""/>*/}
+                {/*<DateList />*/}
             </div>
 
-            <Footers />
+
+            <div style={{width : '100%', maxWidth : 1000, paddingLeft : isDesktop ? 45 : 0}}>
+                <img src="/program_img.png" width={ isDesktop ?' 50%':'100%'} style={{float: 'left'}} alt=""/>
+                <img src="/schedule_img.png" width={isDesktop ? '50%' : '100%'}  style={{float: 'left'}} alt=""/>
+            </div>
+            <div style={{width : '100%', float : 'right'}}>
+                <Footers/>
+            </div>
         </>
     );
 }
